d4b1b7cf12cba5c50c7d353db430cc4badd4ad3f
pirupiruntha <spiruntha1116@gmail.com>
2023-03-20 17:19:33 +0530

    add js & css cdn for using bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplyLeaveComponent } from './components/apply-leave/apply-leave.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MyLeaveComponent } from './components/my-leave/my-leave.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LoginComponent},
+  {path:"navbar", component:NavbarComponent},
+  {path:"home", component:HomeComponent},
+  {path:"applyLeave", component:ApplyLeaveComponent},
+  {path:"myLeave", component:MyLeaveComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ApplyLeaveComponent } from './components/apply-leave/apply-leave.component';
+import { MyLeaveComponent } from './components/my-leave/my-leave.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { LeaveRequestsComponent } from './components/leave-requests/leave-requests.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    ApplyLeaveComponent,
+    MyLeaveComponent,
+    EmployeeDetailsComponent,
+    LeaveRequestsComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
