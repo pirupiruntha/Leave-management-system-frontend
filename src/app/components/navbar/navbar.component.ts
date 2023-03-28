d14d43c7a9e6a7b0f4f13fcefa628af1f1a1ee87
pirupiruntha <spiruntha1116@gmail.com>
2023-03-28 22:18:40 +0530

    create navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  isLoggedIn = false;
+  isUser = false;
+  isAdmin = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    
+  }
+
+
+    userRole: any;
+
+  saveData(){
+      localStorage.setItem('USER_ROLE', "ROLE_ADMIN");
+  }
 }
