377f5e8e93cffa63032aa6f231c351a2713ecdbd
pirupiruntha <spiruntha1116@gmail.com>
2023-03-28 22:24:53 +0530

    add some module and component(Forms , HttpClient)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +16,12 @@ import { MyLeaveComponent } from './components/my-leave/my-leave.component';
 import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 import { LeaveRequestsComponent } from './components/leave-requests/leave-requests.component';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token');
+}
+
 
 @NgModule({
   declarations: [
@@ -23,12 +33,21 @@ import { AddEmployeeComponent } from './components/add-employee/add-employee.com
     MyLeaveComponent,
     EmployeeDetailsComponent,
     LeaveRequestsComponent,
-    AddEmployeeComponent
+    AddEmployeeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        allowedDomains: ['localhost:3000']
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
