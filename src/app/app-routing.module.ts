640e851561e84e5f7173e6e11adb6f7c05ce98e4
pirupiruntha <spiruntha1116@gmail.com>
2023-03-28 22:26:06 +0530

    add some routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 import { ApplyLeaveComponent } from './components/apply-leave/apply-leave.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 import { HomeComponent } from './components/home/home.component';
+import { LeaveRequestsComponent } from './components/leave-requests/leave-requests.component';
 import { LoginComponent } from './components/login/login.component';
 import { MyLeaveComponent } from './components/my-leave/my-leave.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,7 +14,10 @@ const routes: Routes = [
   {path:"navbar", component:NavbarComponent},
   {path:"home", component:HomeComponent},
   {path:"applyLeave", component:ApplyLeaveComponent},
-  {path:"myLeave", component:MyLeaveComponent}
+  {path:"myLeave", component:MyLeaveComponent},
+  {path:"addEmployee", component:AddEmployeeComponent},
+  {path:"employeedetails", component:EmployeeDetailsComponent},
+  {path:"leaveRequests", component:LeaveRequestsComponent}
 ];
 
 @NgModule({
