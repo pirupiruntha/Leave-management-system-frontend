156af5f104d72d16cd854ce3681ef9d2552ab31c
pirupiruntha <spiruntha1116@gmail.com>
2023-03-30 10:34:13 +0530

    create user navbar component

---
+++ src/app/components/user-navbar/navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { StorageService } from 'src/app/services/storage.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  isLoggedIn = false;
+  isUser = false;
+  isAdmin = false;
+  roles: string[] = [];
+
+
+  constructor(private authService: AuthService,  private storageService: StorageService ) {}
+
+  ngOnInit() {
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+    
+  }
+  changeNavbar(){
+    localStorage.getItem("roles")
+  }
+
+}
