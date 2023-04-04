16eb4abd54d7d7978416bb0b07b9b3c29a2524aa
pirupiruntha <spiruntha1116@gmail.com>
2023-04-04 23:48:33 +0530

    connect with api

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-add-employee',
@@ -6,7 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
-  menu1="Employee Details";
-  menu2="Leave Requests"
+  
+  employeeForm = new FormGroup({
 
+    fullName: new FormControl(''),
+    employeeId: new FormControl(''),
+    email: new FormControl(''),
+    dob: new FormControl(''),
+    gender: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    telNo: new FormControl(''),
+    startDate: new FormControl(''),
+    education: new FormControl(''),
+    jobTitle: new FormControl(''),
+    salary: new FormControl('')
+  });
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const employeeData = this.employeeForm.value;
+    console.log(this.employeeForm.value);
+    this.http.post('http://localhost:8080/employees', employeeData).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  // onSubmit() {
+  //   console.log(this.employeeForm.value);
+  //   this.employeeService.addEmployeeData(this.employeeForm.value).subscribe((response) => {
+  //     console.log(response);
+  //   });
+  // }
 }
