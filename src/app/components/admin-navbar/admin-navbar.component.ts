7d42e94a1eb13b38629d1e5573a281ef932634a7
pirupiruntha <spiruntha1116@gmail.com>
2023-04-04 23:51:10 +0530

    modify and add logout function

--- src/app/components/admin-navbar/admin-navbar.component.ts
+++ src/app/components/admin-navbar/admin-navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-admin-navbar',
   templateUrl: './admin-navbar.component.html',
   styleUrls: ['./admin-navbar.component.css']
 })
-export class AdminNavbarComponent {
+export class AdminNavbarComponent implements OnInit {
 
+  constructor(private storageService: StorageService, private route: Router){}
+  ngOnInit(): void {
+  }
+
+  Logout(): void{
+    this.storageService.clean();
+    this.route.navigate([''])
+  }
 }
