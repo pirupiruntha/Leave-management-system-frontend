def8c0498b860f00a16d70a1c994e8eb83528003
pirupiruntha <spiruntha1116@gmail.com>
2023-04-04 23:49:53 +0530

    write getEmployeeByUsername function

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Employee } from 'src/app/model/employee';
+import { EmployeeServiceService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public employees: any = [];
+  public employee:any;
+  public mainModel = new Employee();
+
+  employeeForm = new FormGroup({
+    id: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    
+  });
+
+  constructor(private employeeService :EmployeeServiceService){}
+
+  ngOnInit(): void {
+    this.getEmployeeDataByUsername()
+    
+  }
+
+  getEmployeeDataByUsername(): void { 
+    this.employeeService 
+      .getEmployeeDataByUsername()
+      this.employee
+  }
+
 }
