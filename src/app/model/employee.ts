c701f4b6f8331f2737c18df56a462c6047baf3ce
pirupiruntha <spiruntha1116@gmail.com>
2023-04-04 23:46:40 +0530

    declare employee class variables

--- src/app/model/employee.ts
+++ src/app/model/employee.ts
@@ -1,2 +1,15 @@
 export class Employee {
+    empId!: string
+    dob!: Date
+    fullName!: string
+    email!: string
+    usernane!: string
+    password!: string
+    education!: string
+    gender!: string
+    employeeRole!: string
+    telephoneNo!: string
+    jobTitle!: string
+    salary!: DoubleRange
+    startDate!: Date
 }
