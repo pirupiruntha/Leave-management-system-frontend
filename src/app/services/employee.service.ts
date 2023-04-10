4141c2a5debbd111eecae262cbd2cec16ffbdb9b
pirupiruntha <spiruntha1116@gmail.com>
2023-04-11 00:10:32 +0530

    create methods for apply & update leaves

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -4,6 +4,8 @@ import { Employee } from '../model/employee';
 import { Observable } from 'rxjs';
 import { StorageService } from './storage.service';
 import { LeaveRequest } from '../model/leave-request';
+import { DeleteResponse } from '../model/deleteResponse';
+import { UpdateLeaveRequest } from '../model/updateLeaveRequest';
 
 
 @Injectable({
@@ -21,7 +23,7 @@ export class EmployeeServiceService  {
 
   private getAuthTokenHeader(): HttpHeaders {
     const token = this.storageService.getUser().token;
-    return new HttpHeaders().set('Authorization', `Bearer ${token}`).append('Access-Control-Allow-Origin',"*");  
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);  
   }
 
   addEmployee(employee: Employee): Observable<Employee> {
@@ -29,20 +31,45 @@ export class EmployeeServiceService  {
     return this.http.post<Employee>(`${this.baseUrl}employees`, employee, { headers });
   }
 
-  applyLeave(leaveRequest: LeaveRequest, username: string): Observable<LeaveRequest> {
+  updateEmployee(username:string, employee: Employee): Observable<Employee> {
+    const headers = this.getAuthTokenHeader();
+    return this.http.put<Employee>(`${this.baseUrl}employees/${username}`, employee, { headers });
+  }
+
+  updateLeave(updateLeaveRequest: UpdateLeaveRequest): Observable<LeaveRequest> {
+    const headers = this.getAuthTokenHeader();
+    return this.http.put<LeaveRequest>(`${this.baseUrl}employee/leave`, updateLeaveRequest, { headers });
+  }
+
+  applyLeave(username: string, leaveRequest: LeaveRequest): Observable<LeaveRequest> {
     const headers = this.getAuthTokenHeader();
     return this.http.post<LeaveRequest>(`${this.baseUrl}employee/leave/${username}`, leaveRequest, { headers });
   }
 
   getAllEmployees(): Observable<Employee[]> {
     const headers = this.getAuthTokenHeader();
-    return this.http.get<Employee[]>(`${this.baseUrl}employees`, { headers });
-    
+    return this.http.get<Employee[]>(`${this.baseUrl}employees`, { headers }); 
+  }
+
+  getAllLeaves(): Observable<LeaveRequest[]> {
+    const headers = this.getAuthTokenHeader();
+    return this.http.get<LeaveRequest[]>(`${this.baseUrl}employee/leaves`, { headers }); 
   }
 
   getEmployeeDataByUsername(username: string): Observable<Employee> {
     const headers = this.getAuthTokenHeader();
     return this.http.get<Employee>(`${this.baseUrl}employees/${username}`, { headers });
+  }
+
+  getLeaveRequestByUsername(username: string): Observable<LeaveRequest> {
+    const headers = this.getAuthTokenHeader();
+    return this.http.get<LeaveRequest>(`${this.baseUrl}employee/leave/${username}`, { headers });
+  }
+
+  deleteEmployeeByUsername(username: string): Observable<DeleteResponse> {
+    const headers = this.getAuthTokenHeader();
+    console.log("e delere");
+    return this.http.delete<DeleteResponse>(`${this.baseUrl}employees/${username}`, { headers });
   }
   
 }
