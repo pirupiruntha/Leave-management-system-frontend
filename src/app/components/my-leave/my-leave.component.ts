93460c0d68aba589a3bb9e64da8b5656dacf8e50
pirupiruntha <spiruntha1116@gmail.com>
2023-04-11 14:11:53 +0530

    connect with api

--- src/app/components/my-leave/my-leave.component.ts
+++ src/app/components/my-leave/my-leave.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LeaveRequest } from 'src/app/model/leave-request';
+import { EmployeeServiceService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-my-leave',
   templateUrl: './my-leave.component.html',
   styleUrls: ['./my-leave.component.css']
 })
-export class MyLeaveComponent {
+export class MyLeaveComponent implements OnInit {
+  userLeaveData: any =[]; 
+
+  leaveAllowance ?: string;
+  leaveBalance ?: string;
+  id?: string;
+  startDate ?: Date;
+  endDate?: string;
+  reason?: string;
+  status?: string;
+
+  private username: string='';
+
+  constructor(private employeeService :EmployeeServiceService){}
+
+  ngOnInit(): void {
+    this.username = localStorage.getItem("USERNAME") || "" ;
+    this.getLeavesByUsername(this.username)
+    this.getEmployeeDataByUsername(this.username)
+  }
+
+  getLeavesByUsername(username: string): void{
+    this.employeeService.getLeaveRequestByUsername(username).subscribe((data)=> {
+      this.userLeaveData = data;
+    })
+  }
+
+  getEmployeeDataByUsername(username: string): void { 
+    this.employeeService.getEmployeeDataByUsername(this.username).subscribe((data)=>{
+      this.leaveAllowance = data.leaveAllowance;
+      this.leaveBalance = data.leaveBalance;
+    })
+  }
 
 }
