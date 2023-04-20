5ca4a31848cc59a39d948a1037ba5491a42863a4
pirupiruntha <spiruntha1116@gmail.com>
2023-04-20 17:00:53 +0530

    add form validations

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Employee } from 'src/app/model/employee';
 import { EmployeeServiceService } from 'src/app/services/employee.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -9,8 +9,9 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
+
 export class AddEmployeeComponent implements OnInit {
-  existingEmployee: any ={};
+  existingEmployee: any = {};
   isUpdate = false;
 
   constructor(private employeeService: EmployeeServiceService, private router: Router, private route: ActivatedRoute) { }
@@ -19,38 +20,48 @@ export class AddEmployeeComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       if (params['employee']) {
         this.existingEmployee = JSON.parse(params['employee']);
-        this.isUpdate=true;
+        this.isUpdate = true;
       }
     });
   }
 
   employeeForm = new FormGroup({
     fullName: new FormControl(''),
-    empId: new FormControl(''),
-    email: new FormControl(''),
+    empId: new FormControl('', Validators.maxLength(10)),
+    email: new FormControl('', Validators.email),
     dob: new FormControl(''),
     gender: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl(''),
-    telephoneNo: new FormControl(''),
+    username: new FormControl('', Validators.compose([
+      Validators.minLength(4),
+      Validators.maxLength(10)])),
+    password: new FormControl('', Validators.compose([
+      Validators.minLength(6),
+      Validators.maxLength(10)])),
+    confirmPassword: new FormControl('', Validators.compose([
+      Validators.minLength(6),
+      Validators.maxLength(10)])),
+    telephoneNo: new FormControl('', Validators.compose([
+      Validators.minLength(10),
+      Validators.maxLength(12)])),
     startDate: new FormControl(''),
-    leaveAllowance: new FormControl(''),
+    leaveAllowance: new FormControl('', Validators.compose([Validators.max(21), Validators.min(6)])),
     education: new FormControl(''),
     jobTitle: new FormControl(''),
     salary: new FormControl('')
   });
 
-  errorMessage: string ='';
-  successMessage: string ='';
-  
+
+  errorMessage: string = '';
+  successMessage: string = '';
+
   onSubmit() {
     if (this.employeeForm.invalid) {
       this.errorMessage = "Please fill out all required fields.";
-      return ;
+      return;
     }
-  
+
     const employee: Employee = this.employeeForm.value as Employee;
-    if(this.isUpdate){
+    if (this.isUpdate) {
       this.employeeService.updateEmployee(this.existingEmployee.username, employee).subscribe(
         () => {
           alert("Employee update successfully!");
@@ -63,7 +74,7 @@ export class AddEmployeeComponent implements OnInit {
           this.errorMessage = "Failed to update employee. Please try again later.";
         }
       );
-    }else{
+    } else {
       this.employeeService.addEmployee(employee).subscribe(
         () => {
           alert("Employee added successfully!");
@@ -79,5 +90,10 @@ export class AddEmployeeComponent implements OnInit {
     }
   }
 
-}
+  password(formGroup: FormGroup) {
+    const password = formGroup.get('password');
+    const confirmPassword = formGroup.get('confirmpassword');
+    return password === confirmPassword ? null : { passwordNotMatch: true };
+  }
 
+}
